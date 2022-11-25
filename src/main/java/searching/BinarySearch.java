a2a55949c47a4ecd2e24a77a29d4f44017937d77
petko.lyutskanov <simona2000petko@abv.bg>
2022-11-25 20:23:20 +0200

    Searching - Binary Search implemented

---
+++ src/main/java/searching/BinarySearch.java
@@ -0,0 +1,28 @@
+package searching;
+
+public class BinarySearch {
+
+    public static int binarySearch(int[] arr, int value) {
+        int start = 0;
+        int end = arr.length - 1;
+        int middle = (start + end) / 2;
+
+        while (arr[middle] != value && start <= end) {
+            if (value < arr[middle]) {
+                end = middle - 1;
+            } else {
+                start = middle + 1;
+            }
+
+            middle = (start + end) / 2;
+        }
+
+        if (arr[middle] == value) {
+            System.out.println("The element is found at the index: " + middle);
+            return middle;
+        } else {
+            System.out.println("The element " + value + " is not found");
+            return -1;
+        }
+    }
+}
