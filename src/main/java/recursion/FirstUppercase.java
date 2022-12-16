5ba989fe627fa3d66444d27558cbffd9ff219375
petko.lyutskanov <simona2000petko@abv.bg>
2022-12-16 12:37:45 +0200

    Recursion - First Uppercase task

---
+++ src/main/java/recursion/FirstUppercase.java
@@ -0,0 +1,21 @@
+package recursion;
+
+public class FirstUppercase {
+    public static void main(String[] args) {
+        String str = "soMe Text";
+
+        System.out.println(first(str));
+    }
+
+    public static char first(String str) {
+        if (str.isEmpty()) {
+            return ' ';
+        }
+
+        if (Character.isUpperCase(str.charAt(0))) {
+            return str.charAt(0);
+        } else {
+            return first(str.substring(1, str.length()));
+        }
+    }
+}
