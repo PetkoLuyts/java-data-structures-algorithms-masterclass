5dbb1d51c56d7706836f05aa0a04bc2e50dd98d4
petko.lyutskanov <simona2000petko@abv.bg>
2022-12-17 12:19:07 +0200

    Arrays - 2D Array Diagonal Sum task

---
+++ src/main/java/array/DiagonalSum.java
@@ -0,0 +1,19 @@
+package array;
+
+public class DiagonalSum {
+    public static void main(String[] args) {
+        int[][] myArray2D= {{1,2,3}, {4,5,6}, {7,8,9}};
+
+        System.out.println(sumDiagonal(myArray2D));
+    }
+
+    public static int sumDiagonal(int[][] a) {
+        int sum = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            sum += a[i][i];
+        }
+
+        return sum;
+    }
+}
