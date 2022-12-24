a768f1913b2e02f10241e11f0ac10e57fd6395b9
petko.lyutskanov <simona2000petko@abv.bg>
2022-12-24 11:18:44 +0200

    Trees And Graphs - Route Between Nodes task

---
+++ src/main/java/trees_and_graphs/route_between_nodes/GraphNode.java
@@ -0,0 +1,20 @@
+package trees_and_graphs.route_between_nodes;
+
+import java.util.ArrayList;
+
+public class GraphNode {
+    public String name;
+    public int index;
+    public boolean isVisited = false;
+    public GraphNode parent;
+    public Graph.State state;
+
+    public ArrayList<GraphNode> neighbors = new ArrayList<GraphNode>();
+
+    public GraphNode(String name, int index) {
+        this.name = name;
+        this.index = index;
+    }
+
+
+}
